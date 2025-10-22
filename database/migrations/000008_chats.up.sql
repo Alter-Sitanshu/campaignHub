d61e0238c92da652e409c3204e0a48851d0f3e97
Alter-Sitanshu <sitanshumohapatra5@gmail.com>
2025-10-23 00:43:25 +0530

    bug fix

--- database/migrations/000008_chats.up.sql
+++ database/migrations/000008_chats.up.sql
@@ -3,17 +3,6 @@ CREATE TABLE IF NOT EXISTS conv_status (
     name varchar(10) not null
 );
 
-CREATE TABLE IF NOT EXISTS messages (
-    id varchar(36) primary key,
-    conversation_id varchar(36) not null,
-    sender_id varchar(36) not null,
-    -- I know the tags will always be 3 chars only
-    message_type varchar(3) not null check (message_type IN ('txt', 'pdf', 'img')),
-    content text not null,
-    is_read boolean DEFAULT FALSE,
-    created_at timestamptz DEFAULT now()
-);
-
 CREATE TABLE IF NOT EXISTS conversations (
     id varchar(36) primary key,
     participant_one varchar(36) not null,
@@ -27,8 +16,19 @@ CREATE TABLE IF NOT EXISTS conversations (
     CONSTRAINT fk_conv_status FOREIGN KEY (status) REFERENCES conv_status (id)
 );
 
+CREATE TABLE IF NOT EXISTS messages (
+    id varchar(36) primary key,
+    conversation_id varchar(36) not null,
+    sender_id varchar(36) not null,
+    -- I know the tags will always be 3 chars only
+    message_type varchar(3) not null check (message_type IN ('txt', 'pdf', 'img')),
+    content text not null,
+    is_read boolean DEFAULT FALSE,
+    created_at timestamptz DEFAULT now(),
+
+    CONSTRAINT fk_conversation_id FOREIGN KEY (conversation_id) REFERENCES conversations(id);
+);
+
 -- Populate the status table for conversations
 INSERT INTO conv_status (id, name)
 VALUES (1, 'open'), (0, 'closed');
-
-CONSTRAINT fk_conversation_id FOREIGN KEY (conversation_id) REFERENCES conversations(id);
