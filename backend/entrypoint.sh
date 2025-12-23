029d3e9aabf69afbd17857c0231fa6503dfb63a7
Alter-Sitanshu <sitanshumohapatra5@gmail.com>
2025-12-24 01:37:30 +0530

    deployment nudges

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for postgres..."
-until nc -z db 5432; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-echo "Postgres is up"
+# echo "Waiting for postgres..."
+# until nc -z db 5432; do
+#   echo "Postgres is unavailable - sleeping"
+#   sleep 1
+# done
+# echo "Postgres is up"
 
 echo "Running migrations..."
 /app/migrate -path /app/migrations -database "$DB_ADDR" -verbose up
